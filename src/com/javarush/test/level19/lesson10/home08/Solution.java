49c794e69e3fc6ce1aec5369186aea598bccec0e
stanislasdp <forempty@gmail.com>
2016-06-21 17:05:15 +0300

    Update Solution.java

--- src/com/javarush/test/level19/lesson10/home08/Solution.java
+++ src/com/javarush/test/level19/lesson10/home08/Solution.java
@@ -17,6 +17,22 @@ package com.javarush.test.level19.lesson10.home08;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 String file_name = br.readLine();
+		 br.close();
+		 
+		 BufferedReader br_file = new BufferedReader(new FileReader(file_name));
+		 StringBuilder sb = new StringBuilder();
+		 
+		 while (br_file.ready())
+		 {
+			 sb.append(br_file.readLine());
+		 }
+		 br_file.close();
+		 sb.reverse();
+		 System.out.println(sb.toString());
     }
 }
