36ca287f699d7b5931ec4b83063a424c93b05303
stanislasdp <forempty@gmail.com>
2016-06-21 16:11:30 +0300

    Update Solution.java

--- src/com/javarush/test/level19/lesson10/home06/Solution.java
+++ src/com/javarush/test/level19/lesson10/home06/Solution.java
@@ -23,9 +23,60 @@ import java.util.Map;
 */
 
 public class Solution {
-    public static Map<Integer, String> map = new HashMap<Integer, String>();
+   
+	    public static Map<Integer, String> map = new HashMap<Integer, String>();
+	    static
+	    {
+	    	map.put(0, "ноль");
+	    	map.put(1, "один");
+	    	map.put(2, "два");
+	    	map.put(3, "три");
+	    	map.put(4, "четыре");
+	    	map.put(5, "пять");
+	    	map.put(6, "шесть");
+	    	map.put(7, "семь");
+	    	map.put(8, "восемь");
+	    	map.put(9, "девять");
+	    	map.put(10, "десять");
+	    	map.put(11, "одиннадцать");
+	    	map.put(12, "двенадцать");
+	    }
 
-    public static void main(String[] args) {
+	    public static void main(String[] args) throws IOException 
+	    {
 
-    }
+	    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	    	String file_name =bf.readLine();
+	    	bf.close();
+	    	
+	    	BufferedReader file_reader = new BufferedReader(new FileReader(file_name));
+	    	
+	    	
+	    	while (file_reader.ready())
+	    	{
+	    		String[] splitted = file_reader.readLine().split(" ");
+	    		
+	    		for (int i = 0; i < splitted.length; i++) 
+	    		{
+					for(Map.Entry<Integer, String> pair : map.entrySet())
+					{
+						
+						if (splitted[i].equals(pair.getKey().toString()))
+						{
+							splitted[i]=pair.getValue();			
+						}
+					}
+				}
+	    		for (int j = 0; j < splitted.length; j++) 
+				{
+					System.out.print(splitted[j]);
+					if (j!=splitted.length-1)
+					{
+						System.out.print(" ");
+					}
+				}
+	    		System.out.println("\n");
+	    	}
+	    	
+	    }
 }
