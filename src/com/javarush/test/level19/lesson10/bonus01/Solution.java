9f5df4f3b7125640bc628ab0d6e9584de3554fdc
stas <forempty@gmail.com>
2016-06-22 00:57:23 +0300

    1

--- src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -27,31 +27,7 @@ file1:         file2:             результат:(lines)
 */
 
 public class Solution {
-    public static List<LineItem> lines = new ArrayList<LineItem>();
-
-    public static void main(String[] args) {
-    }
 
 
 
-
-    public static enum Type {
-        ADDED,        //добавлена новая строка
-        REMOVED,      //удалена строка
-        SAME          //без изменений
-    }
-    
-    
-    
-    
-
-    public static class LineItem {
-        public Type type;
-        public String line;
-
-        public LineItem(Type type, String line) {
-            this.type = type;
-            this.line = line;
-        }
-    }
 }
