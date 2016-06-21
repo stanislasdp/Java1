07cd7244d3df6d26fb98a025a07c4467e37416bb
stanislasdp <forempty@gmail.com>
2016-06-21 16:55:23 +0300

    Update Solution.java

--- src/com/javarush/test/level19/lesson10/home07/Solution.java
+++ src/com/javarush/test/level19/lesson10/home07/Solution.java
@@ -11,7 +11,30 @@ package com.javarush.test.level19.lesson10.home07;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
+      BufferedReader br_first = new BufferedReader(new FileReader(args[0]));
+    	BufferedWriter br_second = new BufferedWriter(new FileWriter(args[1]));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	while(br_first.ready())
+    	{
+    		String [] current_string=br_first.readLine().split(" ");
+    		
+   
+    		for (int i = 0; i < current_string.length; i++) 
+    		{
+    			if (current_string[i].length()>6)
+    			{
+    				sb.append(current_string[i]);
+    				sb.append(",");
+    			}	
+			}
+    	}
+    	sb.deleteCharAt(sb.length()-1);
+    	br_second.write(sb.toString());
+    	br_first.close();
+    	br_second.close();
     }
 }
