276eaa73974d33c3568bfa8927de74079b5eda18
stanislasdp <forempty@gmail.com>
2016-06-22 18:30:18 +0300

    Update Solution.java

--- src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -1,7 +1,12 @@
 package com.javarush.test.level19.lesson10.bonus01;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
 
 /* Отслеживаем изменения
 Считать в консоли 2 имени файла - file1, file2.
@@ -12,25 +17,128 @@ import java.util.List;
 оригинальный   редактированный    общий
 file1:         file2:             результат:(lines)
 
-
 строка1        строка1            SAME строка1
 строка2                           REMOVED строка2
 строка3        строка3            SAME строка3
 строка4                           REMOVED строка4
 строка5        строка5            SAME строка5
-строка0                           ADDED строка0
+               строка0            ADDED строка0
 строка1        строка1            SAME строка1
 строка2                           REMOVED строка2
 строка3        строка3            SAME строка3
-строка5                           ADDED строка5
+               строка5            ADDED строка5
 строка4        строка4            SAME строка4
 строка5                           REMOVED строка5
-*/
+ */
 
-public class Solution {
+public class Solution
+{
+	public static List<LineItem> lines = new ArrayList<LineItem>();
 
 
 
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String first_file_name = bf.readLine();
+		String second_file_name = bf.readLine();
+		ArrayList<String> old_file_strings = ReadFiles(first_file_name);
+		ArrayList<String>  new_file_strings = ReadFiles(second_file_name);
+		bf.close();
 
-//testat
+		boolean same_flag = true;
+
+		
+		while(true)
+		{
+			if (old_file_strings.size()==0 || new_file_strings.size()==0)
+			{
+				break;
+			}
+			else
+			{
+				if (old_file_strings.get(0).equals(new_file_strings.get(0)))
+				{
+					lines.add(new LineItem(Type.SAME, old_file_strings.get(0)));
+					old_file_strings.remove(0);
+					new_file_strings.remove(0);
+					same_flag =true;
+				}
+				else if ((old_file_strings.get(0).equals(new_file_strings.get(1))) && same_flag)
+				{
+					lines.add(new LineItem(Type.ADDED, new_file_strings.get(0)));
+					new_file_strings.remove(0);
+					same_flag = false;
+				}
+				else if (old_file_strings.get(1).equals(new_file_strings.get(0))&& same_flag)
+				{
+					lines.add(new LineItem(Type.REMOVED, old_file_strings.get(0)));
+					old_file_strings.remove(0);
+					same_flag = false;
+				}
+				else 
+				{
+					break;
+				}
+			}
+			
+			old_file_strings.trimToSize();
+			new_file_strings.trimToSize();
+			
+			if (old_file_strings.size()==0 && new_file_strings.size()!=0)
+			{
+				lines.add(new LineItem(Type.ADDED, new_file_strings.get(0)));
+			}
+			if (new_file_strings.size()==0 && old_file_strings.size()!=0)
+			{
+				lines.add(new LineItem(Type.REMOVED, old_file_strings.get(0)));
+			}
+			
+	
+		
+		}
+		  for (LineItem item : lines) 
+		{
+			System.out.println(item.line+" "+item.type);
+		}
+
+	}
+			
+		
+
+	 
+	public static enum Type
+	{
+		ADDED,        //added new line
+		REMOVED,      //removed line
+		SAME          //no changes
+	}
+
+	public static class LineItem
+	{
+		public Type type;
+		public String line;
+
+		public LineItem(Type type, String line)
+		{
+			this.type = type;
+			this.line = line;
+		}
+	}
+
+
+	public static ArrayList<String> ReadFiles(String filename) throws IOException
+	{
+		BufferedReader br_file1 = new BufferedReader(new FileReader(filename));
+		ArrayList<String> list_of_strings = new ArrayList<>();
+		while (br_file1.ready())
+		{
+			list_of_strings.add(br_file1.readLine());
+		}
+		br_file1.close();
+		return list_of_strings;
+	}
+
+
 }
+
