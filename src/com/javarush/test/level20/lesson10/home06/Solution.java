9df174901f2687599800fc90206a813762010082
stanislasdp <forempty@gmail.com>
2016-06-27 11:38:02 +0300

    Update Solution.java

--- src/com/javarush/test/level20/lesson10/home06/Solution.java
+++ src/com/javarush/test/level20/lesson10/home06/Solution.java
@@ -11,5 +11,30 @@ public class Solution implements Serializable
     public static class SubSolution extends Solution
     {
 
+        private void writeObject(ObjectOutputStream out) throws IOException
+        {
+           throw new NotSerializableException();
+        }
+        
+        private void readObject(ObjectInputStream in) throws IOException
+        {
+        	throw new NotSerializableException();
+        }
+
     }
-}
+    
+    public static void main(String[] args) throws IOException,ClassNotFoundException
+    {
+    	//FileOutputStream fo = new FileOutputStream("D:\\test\\output.txt");
+    	//ObjectOutputStream oi = new ObjectOutputStream(fi);
+    	
+    //	Solution.SubSolution sub = new Solution.SubSolution();
+    	
+    //	oi.writeObject(sub);
+    	
+    	FileInputStream fi  =new FileInputStream("D:\\test\\output.txt");
+    	ObjectInputStream oi  = new ObjectInputStream(fi);
+    	Solution.SubSolution test;
+    	test = (Solution.SubSolution)oi.readObject();
+    	
+    }
