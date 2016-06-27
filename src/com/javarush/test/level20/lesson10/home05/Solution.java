722c203228b6ce3a5611b5479c65b6beb1b07d09
stas <forempty@gmail.com>
2016-06-27 07:56:24 +0300

    1

--- src/com/javarush/test/level20/lesson10/home05/Solution.java
+++ src/com/javarush/test/level20/lesson10/home05/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.test.level20.lesson10.home05;
 
-import java.io.PrintStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.logging.Logger;
 
 /* Сериализуйте Person
@@ -12,12 +11,12 @@ public class Solution {
     public static class Person implements Serializable{
         String firstName;
         String lastName;
-        String fullName;
-        final String greetingString;
+      transient  String fullName;
+       transient final String greetingString;
         String country;
         Sex sex;
-        PrintStream outputStream;
-        Logger logger;
+         transient PrintStream outputStream;
+         transient Logger logger;
 
         Person(String firstName, String lastName, String country, Sex sex) {
             this.firstName = firstName;
@@ -38,9 +37,21 @@ public class Solution {
     }
 
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException,ClassNotFoundException
     {
+        FileOutputStream fileOutput = new FileOutputStream("/home/stas/test/output.txt");
         Person person1 = new Person("Stas","Kiryan","Ukraine",Sex.MALE);
+
+        ObjectOutputStream ou = new ObjectOutputStream(fileOutput);
+        ou.writeObject(person1);
+        ou.close();
+
+        Person p2;
+        FileInputStream fileInputStream = new FileInputStream("/home/stas/test/output.txt");
+        ObjectInputStream oi = new ObjectInputStream(fileInputStream);
+        p2 = (Person) oi.readObject();
+        System.out.println(p2.firstName);
+        System.out.println(p2.country);
     }
 
 
---
+++ src/com/javarush/test/level20/lesson10/home07/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.test.level20.lesson10.home07;
+
+import java.io.*;
+
+/* Переопределение сериализации в потоке
+Сериализация/десериализация Solution не работает.
+Исправьте ошибки не меняя сигнатуры методов и класса.
+Метод main не участвует в тестировании.
+Написать код проверки самостоятельно в методе main:
+1) создать экземпляр класса Solution
+2) записать в него данные  - writeObject
+3) сериализовать класс Solution  - writeObject(ObjectOutputStream out)
+4) десериализовать, получаем новый объект
+5) записать в новый объект данные - writeObject
+6) проверить, что в файле есть данные из п.2 и п.5
+*/
+public class Solution implements Serializable, AutoCloseable {
+    private FileOutputStream stream;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.stream = new FileOutputStream(fileName);
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+
+
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        Solution sol = new Solution("/home/stas/test/output.txt");
+        sol.writeObject("test");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream outputStream = new ObjectOutputStream(out);
+
+        outputStream.writeObject(sol);
+        outputStream.close();
+
+      //  sol.writeObject(out);
+        ;
+    }
+}
