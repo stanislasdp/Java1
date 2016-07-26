de56f5ec7c752e31504cabfca5312958d2229f9c
stanislasdp <forempty@gmail.com>
2016-07-26 16:47:39 +0300

    Update Solution.java

--- src/com/javarush/test/level24/lesson09/task02/Solution.java
+++ src/com/javarush/test/level24/lesson09/task02/Solution.java
@@ -17,93 +17,130 @@ import java.util.*;
 Fake Apple Inc.   AAPL | 17-11-2025 open 125,64 and last 123,43
 Fake Oracle Corporation   ORCL | 21-08-1989 closed 0,15
 */
-public class Solution {
-    public static void main(String[] args) {
-        List<Stock> stocks = getStocks();
-        sort(stocks);
-        Date actualDate = new Date();
-        printStocks(stocks, actualDate);
-    }
+public class Solution 
+{
+    
+	 public static void main(String[] args) 
+	 {
+	        List<Stock> stocks = getStocks();
+	        sort(stocks);
+	        Date actualDate = new Date();
+	        printStocks(stocks, actualDate);
+	    }
+	 
+	    public static void printStocks(List<Stock> stocks, Date actualDate) 
+	    {
+	        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	        // array of limits
+	        double[] filelimits = {0d, actualDate.getTime()};
+	        // array of formats
+	        String[] filepart = {"closed {4}", "open {2} and last {3}"};
+	        /// how it works?
+	        ChoiceFormat fileform = new ChoiceFormat(filelimits, filepart);
+	        Format[] testFormats = {null,null, dateFormat, fileform};
+	        MessageFormat pattform = new MessageFormat("{0} {1} | {5} {6}");
+	        pattform.setFormats(testFormats);
 
-    public static void printStocks(List<Stock> stocks, Date actualDate) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	        for (Stock stock : stocks) 
+	        {
+	            String name = ((String) stock.get("name"));
+	            String symbol = (String) stock.get("symbol");
+	            double open = !stock.containsKey("open") ? 0 : ((double) stock.get("open"));
+	            double last = !stock.containsKey("last") ? 0 : ((double) stock.get("last"));
+	            double change = !stock.containsKey("change") ? 0 : ((double) stock.get("change"));
+	            Date date = (Date) stock.get("date");
+	            Object[] testArgs = {name, symbol, open, last, change, date, date.getTime()};
+	            System.out.println(pattform.format(testArgs));
+	        }
+	    }
 
-        double[] filelimits = {0d, actualDate.getTime()};
-        String[] filepart = {"closed {4}", "open {2} and last {3}"};
+	    public static void sort(List<Stock> list) {
+	        Collections.sort(list, new Comparator<Stock>() 
+	        		{
+	        	@Override
+	            public int compare(Stock stock1, Stock stock2) 
+	        	{
+	        		int flag = stock1.get("name").toString().compareTo(stock2.get("name").toString());
+	        		if (flag!= 0)
+	        		{
+	        			return flag;
+	        		}
+	        		Calendar cal = Calendar.getInstance();
+	        		cal.setTime((Date)stock1.get("date"));
+	        		int first_year = cal.get(Calendar.YEAR);
+	        		int first_month = cal.get(Calendar.MONTH);
+	        		cal.setTime((Date)stock2.get("date"));
+	        		int second_year = cal.get(Calendar.YEAR);
+	        		int second_month = cal.get(Calendar.MONTH);
+	        		
+	        		flag = second_year - first_year;
+	        		if (flag!=0)
+	        		{
+	        			return flag;
+	        		}
+	        		flag = second_month - first_month;
+	        		if (flag!=0)
+	        		{
+	        			return flag;
+	        		}
+	        		Double first_income = stock1.containsKey("change")?(Double)stock1.get("change"):(Double)stock1.get("last")- (Double)stock1.get("open");
+	        		Double second_income = stock2.containsKey("change")?(Double)stock2.get("change"):(Double)stock2.get("last")- (Double)stock2.get("open");
+	        		flag = second_income.compareTo(first_income);
+	        		return flag;
+	            }
+	        });
+	    }
 
-        ChoiceFormat fileform = new ChoiceFormat(filelimits, filepart);
-        Format[] testFormats = {null, dateFormat, fileform};
-        MessageFormat pattform = new MessageFormat("{0}   {1} | {5} {6}");
-        pattform.setFormats(testFormats);
+	    public static class Stock extends HashMap 
+	    {
+	        public Stock(String name, String symbol, double open, double last) {
+	            put("name", name);
+	            put("symbol", symbol);
+	            put("open", open);
+	            put("last", last);
+	           put("date", getRandomDate(2020));
+	        }
 
-        for (Stock stock : stocks) {
-            String name = ((String) stock.get("name"));
-            String symbol = (String) stock.get("symbol");
-            double open = !stock.containsKey("open") ? 0 : ((double) stock.get("open"));
-            double last = !stock.containsKey("last") ? 0 : ((double) stock.get("last"));
-            double change = !stock.containsKey("change") ? 0 : ((double) stock.get("change"));
-            Date date = (Date) stock.get("date");
-            Object[] testArgs = {name, symbol, open, last, change, date, date.getTime()};
-            System.out.println(pattform.format(testArgs));
-        }
-    }
+	        public Stock(String name, String symbol, double change, Date date) 
+	        {
+	            put("name", name);
+	            put("symbol", symbol);
+	            put("date", date);
+	            put("change", change);
+	        }
+	    }
 
-    public static void sort(List<Stock> list) {
-        Collections.sort(list, new Comparator<Stock>() {
-            public int compare(Stock stock1, Stock stock2) {
-                return 0;
-            }
-        });
-    }
+	    public static List<Stock> getStocks() 
+	    { 
+	        List<Stock> stocks = new ArrayList();
+	        stocks.add(new Stock("Fake Apple Inc.", "AAPL", 125.64, 123.43));
+	        stocks.add(new Stock("Fake Cisco Systems, Inc.", "CSCO", 25.84, 26.3));
+	        stocks.add(new Stock("Fake Google Inc.", "GOOG", 516.2, 512.6));
+	        stocks.add(new Stock("Fake Intel Corporation", "INTC", 21.36, 21.53));
+	        stocks.add(new Stock("Fake Level 3 Communications, Inc.", "LVLT", 5.55, 5.54));
+	        stocks.add(new Stock("Fake Microsoft Corporation", "MSFT", 29.56, 29.72));
 
-    public static class Stock extends HashMap {
-        public Stock(String name, String symbol, double open, double last) {
-            put("name", name);
-            put("symbol", symbol);
-            put("open", open);
-            put("last", last);
-            put("date", getRandomDate(2020));
-        }
+	        stocks.add(new Stock("Fake Nokia Corporation (ADR)", "NOK", .1, getRandomDate()));
+	        stocks.add(new Stock("Fake Oracle Corporation", "ORCL", .15, getRandomDate()));
+	        stocks.add(new Stock("Fake Starbucks Corporation", "SBUX", .03, getRandomDate()));
+	        stocks.add(new Stock("Fake Yahoo! Inc.", "YHOO", .32, getRandomDate()));
+	        stocks.add(new Stock("Fake Applied Materials, Inc.", "AMAT", .26, getRandomDate()));
+	        stocks.add(new Stock("Fake Comcast Corporation", "CMCSA", .5, getRandomDate()));
+	        stocks.add(new Stock("Fake Sirius Satellite", "SIRI", -.03, getRandomDate()));
+	        return stocks;
+	    }
 
-        public Stock(String name, String symbol, double change, Date date) {
-            put("name", name);
-            put("symbol", symbol);
-            put("date", date);
-            put("change", change);
-        }
-    }
+	    public static Date getRandomDate() {
+	        return getRandomDate(1970);
+	    }
 
-    public static List<Stock> getStocks() {
-        List<Stock> stocks = new ArrayList();
-
-        stocks.add(new Stock("Fake Apple Inc.", "AAPL", 125.64, 123.43));
-        stocks.add(new Stock("Fake Cisco Systems, Inc.", "CSCO", 25.84, 26.3));
-        stocks.add(new Stock("Fake Google Inc.", "GOOG", 516.2, 512.6));
-        stocks.add(new Stock("Fake Intel Corporation", "INTC", 21.36, 21.53));
-        stocks.add(new Stock("Fake Level 3 Communications, Inc.", "LVLT", 5.55, 5.54));
-        stocks.add(new Stock("Fake Microsoft Corporation", "MSFT", 29.56, 29.72));
-
-        stocks.add(new Stock("Fake Nokia Corporation (ADR)", "NOK", .1, getRandomDate()));
-        stocks.add(new Stock("Fake Oracle Corporation", "ORCL", .15, getRandomDate()));
-        stocks.add(new Stock("Fake Starbucks Corporation", "SBUX", .03, getRandomDate()));
-        stocks.add(new Stock("Fake Yahoo! Inc.", "YHOO", .32, getRandomDate()));
-        stocks.add(new Stock("Fake Applied Materials, Inc.", "AMAT", .26, getRandomDate()));
-        stocks.add(new Stock("Fake Comcast Corporation", "CMCSA", .5, getRandomDate()));
-        stocks.add(new Stock("Fake Sirius Satellite", "SIRI", -.03, getRandomDate()));
-
-        return stocks;
-    }
-
-    public static Date getRandomDate() {
-        return getRandomDate(1970);
-    }
-
-    public static Date getRandomDate(int startYear) {
-        int year = startYear + (int) (Math.random() * 30);
-        int month = (int) (Math.random() * 12);
-        int day = (int) (Math.random() * 28);
-        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
-        return calendar.getTime();
-    }
+	    public static Date getRandomDate(int startYear) 
+	    {
+	        int year = startYear + (int) (Math.random() * 30);
+	        int month = (int) (Math.random() * 12);
+	        int day = (int) (Math.random() * 28);
+	        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+	        return calendar.getTime();
+	}
 }
 
