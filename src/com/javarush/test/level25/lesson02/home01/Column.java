3b046e8d1a94fa40596ae6a05427d3b461764f25
stas <forempty@gmail.com>
2016-07-28 21:49:20 +0300

    2

--- src/com/javarush/test/level25/lesson02/home01/Column.java
+++ src/com/javarush/test/level25/lesson02/home01/Column.java
@@ -2,103 +2,105 @@ package com.javarush.test.level25.lesson02.home01;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public enum Column implements Columnable
 
 {
-   Customer("Customer"),
-	    BankName("Bank Name"),
-	    AccountNumber("Account Number"),
-	    Amount("Available Amount");
+	Customer("Customer"),
+	BankName("Bank Name"),
+	AccountNumber("Account Number"),
+	Amount("Available Amount");
 
-	    private String columnName;
+	private String columnName;
 
-	    private static int[] realOrder;
+	private static int[] realOrder;
 
-	    private Column(String columnName)
-	    {
-	        this.columnName = columnName;
-	    }
+	private Column(String columnName)
+	{
+		this.columnName = columnName;
+	}
 
-	    /**
-	     * Задает новый порядок отображения колонок, который хранится в массиве realOrder.
-	     * realOrder[индекс в энуме] = порядок отображения; -1, если колонка не отображается.
-	     *
-	     * @param newOrder новая последовательность колонок, в которой они будут отображаться в таблице
-	     * @throws IllegalArgumentException при дубликате колонки
-	     */
-	    public static void configureColumns(Column... newOrder)
-	    {
-	        realOrder = new int[values().length];
-	        for (Column column : values())
-	        {
-	            realOrder[column.ordinal()] = -1;
-	            boolean isFound = false;
+	/**
+	 * Задает новый порядок отображения колонок, который хранится в массиве realOrder.
+	 * realOrder[индекс в энуме] = порядок отображения; -1, если колонка не отображается.
+	 *
+	 * @param newOrder новая последовательность колонок, в которой они будут отображаться в таблице
+	 * @throws IllegalArgumentException при дубликате колонки
+	 */
+	public static void configureColumns(Column... newOrder)
+	{
+		realOrder = new int[values().length];
+		for (Column column : values())
+		{
+			realOrder[column.ordinal()] = -1;
+			boolean isFound = false;
 
-	            for (int i = 0; i < newOrder.length; i++)
-	            {
-	                if (column == newOrder[i])
-	                {
-	                    if (isFound) {
-	                        throw new IllegalArgumentException("Column '" + column.columnName + "' is already configured.");
-	                    }
-	                    realOrder[column.ordinal()] = i;
-	                    isFound = true;
-	                }
-	            }
-	        }
-	    }
-
-	    public String getColumnName()
-	    {
-	       return columnName;
-	    }
-
-	    public  boolean isShown()
-	    {
-	    	if (realOrder[this.ordinal()]!= -1)
-	    	{
-	    		 return true;
-	    	}
-	    	else
-	    	{
-	    		return false;
-	    	}
-	       
-	    }
-
-	    public void hide()
-	    {
-	    	if (isShown())
-	    	{
-	    		realOrder[ordinal()] = -1;
-	    	}
-	    	
-	    	
-	        /**
-	         * Скрывает колонку - маркирует колонку -1 в массиве realOrder.
-	         * Сдвигает индексы отображаемых колонок, которые идут после скрытой
-	         */
-	        
-	    }
-
-	    /**
-	     * Вычисляет и возвращает список отображаемых колонок в сконфигурированом порядке (см. метод configureColumns)
-	     * Используется поле realOrder.
-	     *
-	     * @return список колонок
-	     */
-	    public static List<Column> getVisibleColumns()
-	    {
-	    	List<Column> result = new LinkedList<>();
-	    	Column[] columns = values();
-	    	for (int i = 0; i < realOrder.length; i++) 
-	    	{
-				if (columns[i].isShown())
+			for (int i = 0; i < newOrder.length; i++)
+			{
+				if (column == newOrder[i])
 				{
-					result.add(Column.values()[realOrder[i]]);
+					if (isFound) {
+						throw new IllegalArgumentException("Column '" + column.columnName + "' is already configured.");
+					}
+					realOrder[column.ordinal()] = i;
+					isFound = true;
 				}
 			}
-	        return result;
+		}
+	}
+
+	public String getColumnName()
+	{
+		return columnName;
+	}
+
+	public  boolean isShown()
+	{
+		return realOrder[this.ordinal()]!= -1;
+
+
+	}
+
+	public void hide()
+	{
+		if (isShown())
+		{
+			realOrder[ordinal()] = -1;
+		}
+
+
+		/**
+		 * Скрывает колонку - маркирует колонку -1 в массиве realOrder.
+		 * Сдвигает индексы отображаемых колонок, которые идут после скрытой
+		 */
+
+	}
+
+	/**
+	 * Вычисляет и возвращает список отображаемых колонок в сконфигурированом порядке (см. метод configureColumns)
+	 * Используется поле realOrder.
+	 *
+	 * @return список колонок
+	 */
+	public static List<Column> getVisibleColumns()
+	{
+		List<Column> result = new LinkedList<>();
+		Column [] val = Column.values();
+
+		Map<Integer,Column> mp = new TreeMap<>();
+
+		for (int i = 0; i < realOrder.length ; i++)
+		{
+			if (val[i].isShown())
+				mp.put(realOrder[i],val[i]);
+		}
+
+		for (Map.Entry<Integer,Column> pair : mp.entrySet())
+		{
+			result.add(pair.getValue());
+		}
+		return result;
 	}
 }
