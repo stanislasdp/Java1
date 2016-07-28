14f3856978f606b97e9e5bf37422a17901987581
stanislasdp <forempty@gmail.com>
2016-07-28 15:45:17 +0300

    Update Solution.java

--- src/com/javarush/test/level25/lesson05/task02/Solution.java
+++ src/com/javarush/test/level25/lesson05/task02/Solution.java
@@ -10,25 +10,51 @@ super secret key, Thread-0, it's an example
 */
 public class Solution {
 
-    public static void main(String[] args) {
-        MyThread myThread = new Solution().new MyThread("super secret key");
-        myThread.start();
-    }
+   public static void main(String[] args) throws InterruptedException 
+	 {
+	        MyThread myThread = new Solution().new MyThread("super secret key");
+	        myThread.start();
+	       
+	    }
 
-    public class MyThread extends Thread {
-        private String secretKey;
+	    public class MyThread extends Thread 
+	    {
+	        private String secretKey;
 
-        public MyThread(String secretKey) {
-            this.secretKey = secretKey;
-            setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
-            setDaemon(true);
-        }
+	        public MyThread(String secretKey) 
+	        {
+	            this.secretKey = secretKey;
+	            setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+	          
+	        }
 
-        @Override
-        public void run() {
-            throw new NullPointerException("it's an example");
-        }
-    }
+	        @Override
+	        public void run() {
+	            throw new NullPointerException("it's an example");
+	        }
+	    }
+	    
+	    private class MyUncaughtExceptionHandler implements UncaughtExceptionHandler
+	    {
+
+			@Override
+			public void uncaughtException(Thread t, Throwable e) 
+			{
+			
+				try
+				{
+					Thread.sleep(500);
+				
+				}
+				catch (InterruptedException ie)
+				{
+					ie.printStackTrace();
+				}
+				String to_console = String.format("%s, %s, %s", ((MyThread)t).secretKey,t.getName(),e.getMessage());
+				System.out.println(to_console);
+		
+			}
+	    }
 
 }
 
