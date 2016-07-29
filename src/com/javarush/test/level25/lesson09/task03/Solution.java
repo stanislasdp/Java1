fc2f0c04885b42c3dcca4f270cda9de790ceb775
stas <forempty@gmail.com>
2016-07-29 21:50:28 +0300

    2

--- src/com/javarush/test/level25/lesson09/task03/Solution.java
+++ src/com/javarush/test/level25/lesson09/task03/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level25.lesson09.task03;
 
+import java.util.ArrayList;
+
 /* Живем своим умом
 В классе Solution реализуйте интерфейс UncaughtExceptionHandler, который должен:
 1. прервать нить, которая бросила исключение.
@@ -10,8 +12,31 @@ java.lang.IllegalAccessException: GHI
 java.lang.RuntimeException: DEF
 java.lang.Exception: ABC
 */
-public class Solution implements Thread.UncaughtExceptionHandler {
+public class Solution implements Thread.UncaughtExceptionHandler
+{
     @Override
-    public void uncaughtException(Thread t, Throwable e) {
+    public void uncaughtException(Thread t, Throwable e)
+    {
+        t.interrupt();
+        Throwable thr = e;
+        ArrayList<Throwable> arr1 = new ArrayList<>();
+        while (thr != null)
+        {
+            arr1.add(0, thr);
+            thr = thr.getCause();
+        }
+
+        for (Throwable th : arr1)
+        {
+            System.out.println(th.toString());
+        }
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        Throwable e = new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")));
+        new Solution().uncaughtException(Thread.currentThread(), e);
     }
 }
