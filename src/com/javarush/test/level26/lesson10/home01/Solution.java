6b3f69d1905a47485c87eb70f6f011c81206c80d
stas <Mustafa89>
2016-08-03 22:35:19 +0300

    4

---
+++ src/com/javarush/test/level26/lesson10/home01/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level26.lesson10.home01;
+
+/* Мир скучен для скучных людей.
+Разберитесь с BlockingQueue
+По образу и подобию класса Producer создайте класс Consumer, который будет выводить данные из BlockingQueue в консоль
+*/
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        BlockingQueue queue = new ArrayBlockingQueue(32);
+
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(producer);
+        executorService.submit(consumer);
+
+        Thread.sleep(2000);
+
+        executorService.shutdownNow();
+
+    }
+}
