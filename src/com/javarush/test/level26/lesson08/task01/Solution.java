946dccfe59fd2489d3481fc190ad582a7862e503
stas <forempty@gmail.com>
2016-08-06 17:47:01 +0300

    4

--- src/com/javarush/test/level26/lesson08/task01/Solution.java
+++ src/com/javarush/test/level26/lesson08/task01/Solution.java
@@ -6,6 +6,56 @@ IntegerHolder должен быть для типа int, быть нитебез
 В этом классе должны быть два public метода get и set
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        final IntegerHolder ih = new IntegerHolder();
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                ih.set(8);
+            }
+
+        }).start();
+        try
+        {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException ie)
+        {
+            ie.printStackTrace();
+        }
+
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                ih.set(7);
+            }
+        }).start();
+
+        System.out.println(ih.get());
+
+
     }
+    public static class IntegerHolder
+    {
+        private int value;
+
+
+        public synchronized int get()
+        {
+            return value;
+        }
+
+        public synchronized void set(int value)
+        {
+            this.value = value;
+        }
+
+    }
+
 }
