88ce04478f7ebe9e15e6855b4665bfb109b174e6
stanislasdp <forempty@gmail.com>
2016-08-09 18:13:15 +0300

    Update WithdrawCommand.java

--- src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
@@ -8,13 +8,12 @@ import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationExce
 /**
  * Created by stas on 8/7/16.
  */
-class WithdrawCommand implements Command
+public class WithdrawCommand implements Command
 {
-
-    @Override
-    public void execute() throws InterruptOperationException
-    {
-        String currency = ConsoleHelper.askCurrencyCode();
+	 @Override
+	    public void execute() throws InterruptOperationException
+	    {
+	        String currency = ConsoleHelper.askCurrencyCode();
 	        CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currency);
 
 	        String sum = "";
@@ -40,8 +39,8 @@ class WithdrawCommand implements Command
 	        	   Map <Integer,Integer> withdrawmap = currencyManipulator.withdrawAmount(Integer.parseInt(sum));
 	        	   for (Map.Entry<Integer, Integer> pair : withdrawmap.entrySet()) 
 	        	   {
-					System.out.println(pair.getKey()+" "+pair.getValue());
-				}
+					System.out.println("\t"+pair.getKey()+" - "+pair.getValue());
+				    }
 	        	   break;
 	           }
 	           catch (NotEnoughMoneyException ne)
@@ -51,27 +50,6 @@ class WithdrawCommand implements Command
 	           
 	          
 	        }
-        }
-
-
- /*       1. Реализуйте следующий алгоритм для команды WithdrawCommand:
-        1.1. Считать код валюты (метод уже есть)
-        1.2. Получить манипулятор для этой валюты.
-        1.3. Пока пользователь не введет корректные данные выполнять:
-        1.3.1. Попросить ввести сумму
-        1.3.2. Если введены некорректные данные, то сообщить об этом пользователю и вернуться к п. 1.3.
-        1.3.3. Проверить, достаточно ли средств на счету.
-        Для этого в манипуляторе создайте метод boolean isAmountAvailable(int expectedAmount), который вернет true, если денег достаточно для выдачи.
-        Если недостаточно, то вернуться к п. 1.3.
-        1.3.4. Списать деньги со счета. Для этого в манируляторе создайте метод
-        Map<Integer, Integer> withdrawAmount(int expectedAmount), который вернет карту Map<номинал, количество>.
-        Подробно логику этого метода смотрите в п.2.
-        1.3.5. Вывести пользователю результат из п. 1.3.4. в следующем виде:
-        <табуляция>номинал - количество
-        Сортировка - от большего номинала к меньшему.
-        Вывести сообщение об успешной транзакции.
-        1.3.6. Перехватить исключение NotEnoughMoneyException, уведомить юзера о нехватке банкнот и вернуться к п. 1.3.*/
-    }
-
-
+	}
+	
 }
