4d1c39ce0ad1e8d563e7870a9a7ddeca9d06187f
stanislasdp <forempty@gmail.com>
2016-08-15 10:52:58 +0300

    Update TransferObject.java

--- src/com/javarush/test/level27/lesson09/home01/TransferObject.java
+++ src/com/javarush/test/level27/lesson09/home01/TransferObject.java
@@ -1,16 +1,37 @@
 package com.javarush.test.level27.lesson09.home01;
 
-public class TransferObject {
-    private int value;
+public class TransferObject 
+{
+	private int value;
     protected volatile boolean isValuePresent = false; //use this variable
 
-    public synchronized int get() {
-        System.out.println("Got: " + value);
+    public synchronized int get() 
+    {
+    	while(isValuePresent)
+    	{
+    		 System.out.println("Got: " + value);
+    		 isValuePresent = false;
+    	}
+       
+        this.notify();
         return value;
     }
 
-    public synchronized void put(int value) {
+    public synchronized void put(int value) 
+    {
         this.value = value;
         System.out.println("Put: " + value);
-    }
+        isValuePresent = true;
+        while(isValuePresent)
+        {
+        	try {
+				this.wait();
+			} catch (InterruptedException e) 
+        	{
+				e.printStackTrace();
+			}
+        }
+        
+}
+  
 }
