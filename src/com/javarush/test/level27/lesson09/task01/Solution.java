6afddb5889eac388e7ba454a4a87f94e499ace2b
stanislasdp <forempty@gmail.com>
2016-08-15 09:45:02 +0300

    Update Solution.java

--- src/com/javarush/test/level27/lesson09/task01/Solution.java
+++ src/com/javarush/test/level27/lesson09/task01/Solution.java
@@ -7,24 +7,20 @@ import java.util.concurrent.CountDownLatch;
 Почитайте про CountDownLatch и перепишите тело метода someMethod используя поле latch.
 Весь лишний код удалите из класса.
 */
-public abstract class Solution {
-    private final Object lock = new Object();
-    private volatile boolean isWaitingForValue = true;
-
+public abstract class Solution 
+{
+	
+    
     private final CountDownLatch latch = new CountDownLatch(1);
 
-    public void someMethod() throws InterruptedException {
-        synchronized (lock) {
-            while (isWaitingForValue) {
-                lock.wait();
-            }
-        }
-
-        retrieveValue();
-
-        isWaitingForValue = false;
-        lock.notify();
+    public void someMethod() throws InterruptedException 
+    {
+     
+        	latch.await();
+        	retrieveValue();
+        	latch.countDown();
     }
 
     abstract void retrieveValue();
 }
+
