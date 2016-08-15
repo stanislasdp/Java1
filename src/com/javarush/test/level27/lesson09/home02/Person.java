a5dcc66e48641bd22262ba5dfc70a60e0e612268
stanislasdp <forempty@gmail.com>
2016-08-15 12:22:28 +0300

    Update Person.java

--- src/com/javarush/test/level27/lesson09/home02/Person.java
+++ src/com/javarush/test/level27/lesson09/home02/Person.java
@@ -1,7 +1,8 @@
 package com.javarush.test.level27.lesson09.home02;
 
-public class Person implements Runnable {
-    private final Mail mail;
+public class Person implements Runnable
+{
+	private final Mail mail;
 
     public Person(Mail mail) {
         this.mail = mail;
@@ -12,11 +13,18 @@ public class Person implements Runnable {
         String name = Thread.currentThread().getName();
         try {
             Thread.sleep(1000);
+            synchronized (mail) 
+            {	
+            	mail.setText("Person [" + name + "] has written an email 'AAA'");
+            	mail.notifyAll();
+			}
             //сделайте что-то тут - do something here
-            mail.setText("Person [" + name + "] has written an email 'AAA'");
+            
             //сделайте что-то тут - do something here
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
 }
+
+}
+
