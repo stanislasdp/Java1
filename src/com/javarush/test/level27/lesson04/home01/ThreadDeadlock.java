732811d4b94853987f5d2c6959061d6dba264f7b
stas <forempty@gmail.com>
2016-08-17 22:55:22 +0300

    2

--- src/com/javarush/test/level27/lesson04/home01/ThreadDeadlock.java
+++ src/com/javarush/test/level27/lesson04/home01/ThreadDeadlock.java
@@ -1,8 +1,14 @@
 package com.javarush.test.level27.lesson04.home01;
 
-public class ThreadDeadlock {
+public class ThreadDeadlock
+{
     Object data;
-    public Object getData() {
-        return data;
+    public synchronized Object getData()
+    {
+        synchronized (data)
+        {
+            return data;
+        }
+
     }
 }
