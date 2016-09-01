433f45e8fd5a707a071b7cbe532ccc7cfb010859
stanislasdp <forempty@gmail.com>
2016-09-01 10:19:42 +0300

    Update Server.java

--- src/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/com/javarush/test/level30/lesson15/big01/Server.java
@@ -5,4 +5,41 @@ package com.javarush.test.level30.lesson15.big01;
  */
 public class Server
 {
+  private static class Handler extends Thread
+	{
+		private Socket socket;
+		
+		Handler(Socket socket)
+		{
+			this.socket = socket;
+		}
+		
+		@Override
+		public void run() 
+		{
+			//some logic will be here
+		}
+	}
+	
+	
+	public static void main(String...args) throws IOException
+	{
+		int serverPort = ConsoleHelper.readInt();
+		
+		
+		try (ServerSocket serverSocket = new ServerSocket(serverPort))
+		{
+			ConsoleHelper.writeMessage("Server is run");
+			while(true)
+			{
+				serverSocket.accept();
+				new Handler(serverSocket.accept()).start();
+			}
+		}
+		catch (Exception e) 
+		{
+			ConsoleHelper.writeMessage("Some socket occured");
+		}
+		
+		}		
 }
