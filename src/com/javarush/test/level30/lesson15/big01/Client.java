187382d80c69aa28dbee99823b84ae886858c73f
stanislasdp <forempty@gmail.com>
2016-09-01 15:05:39 +0300

    Update Client.java
    12

--- src/com/javarush/test/level30/lesson15/big01/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/Client.java
@@ -1,5 +1,13 @@
 package level30_chat.client;
 
-public class Client {
+public class Client 
+{
+  volatile boolean clientConnected = false;
+
+	public class SocketThread extends Thread//12.3
+	{
+		protected Connection connection;//12.4
+		
+	}
 
 }
