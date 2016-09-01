86a9052e9004c6497c439bdb6d1243a70a8074ad
stanislasdp <forempty@gmail.com>
2016-09-01 16:54:05 +0300

    Update Client.java
    14

--- src/com/javarush/test/level30/lesson15/big01/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/Client.java
@@ -9,39 +9,39 @@ import level30_chat.MessageType;
 
 public class Client//12.2 
 {
-	private volatile boolean clientConnected = false;//12.4
+private volatile boolean clientConnected = false;//12.4
 	protected Connection connection;
-	
-	
+
+
 	protected String getServerAddress()//13.1
 	{
 		ConsoleHelper.writeMessage("Enter server address");
 		return ConsoleHelper.readString();
 	}
-	
+
 	protected int getServerPort()//13.2
 	{
 		ConsoleHelper.writeMessage("Enter server port");
 		return ConsoleHelper.readInt();
 	}
-	
+
 	protected String getUserName()//13.3
 	{
 		ConsoleHelper.writeMessage("Enter user name");
 		return ConsoleHelper.readString();
 	}
-	
-	
+
+
 	protected boolean shouldSentTextFromConsole()//13.4
 	{
 		return true;
 	}
-	
+
 	protected SocketThread getSocketThread()
 	{
 		return new SocketThread();//13.5
 	}
-	
+
 	protected void sendTextMessage(String text)//13.6
 	{
 		try
@@ -55,12 +55,62 @@ public class Client//12.2
 			clientConnected = false;
 		}
 	}
-	
-	
-	
-	public class SocketThread extends Thread//12.3
+
+	public void run()
 	{
-		
-		
+		SocketThread socketThread = new SocketThread();//14.1.1
+		socketThread.setDaemon(true);//14.1.2
+		socketThread.start();//14.1.3
+		// when main thread finishes, additional SocketThread thread finishes as well.
+
+		synchronized(this)
+		{
+			try
+			{
+				this.wait();//14.1.4
+			}
+			catch (InterruptedException ie)
+			{
+				ConsoleHelper.writeMessage("Error occured in client");
+				return;
+			}
+		}
+
+			if (clientConnected == true)//14.1.5
+			{
+				ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+			}
+			else
+			{
+				ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+			}
+			
+			String readString=null;
+			while (clientConnected)
+			{
+				readString = ConsoleHelper.readString();
+				if (readString.equalsIgnoreCase("exit"))//14.1.6
+				{
+					break;
+				}
+				if (shouldSentTextFromConsole())
+				{
+					sendTextMessage(readString);//14.1.7
+				}
+			}
+		}
+	
+	
+	public static void main (String...args)
+	{
+		Client client = new Client();//14.2
+		client.run();
 	}
+
+
+		public class SocketThread extends Thread//12.3
+		{
+
+
+		}
 }
