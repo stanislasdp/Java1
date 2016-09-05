c780d38a3c702d4f8b41ce4dd66a4fd932c9b2d0
stas <forempty@gmail.com>
2016-09-05 22:51:55 +0300

    4

--- src/com/javarush/test/level31/lesson02/home01/Solution.java
+++ src/com/javarush/test/level31/lesson02/home01/Solution.java
@@ -1,8 +1,10 @@
 package com.javarush.test.level31.lesson02.home01;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
 
 /* Проход по дереву файлов
 1. На вход метода main подаются два параметра.
@@ -19,31 +21,95 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args)
     {
-        String directoryPath =  args[0];
-        File resultFile= new File(args[1]);
+        String directoryPath = args[0];
+        String resultFileName = args[1];
 
-    }
+        ArrayList<String> fileList = new ArrayList<>();
+        ArrayList<String> resultList = new ArrayList<>();
+        processDirectories(directoryPath,fileList);
 
-
-    public static void processFileTree(String directoryPath, List<String> fileNames)
-    {
-        List<String> filearList = new ArrayList<>();
-
-        File curFile = new File(directoryPath);
-        if (curFile.isDirectory())
+        for (String el :fileList)
         {
-            for (File file : curFile.listFiles())
+            File file = new File(el);
+            if (file.length() > 50)
             {
-                if (file.isDirectory())
-                {
+                file.delete();
+            }
+            else
+            {
+                resultList.add(el);
+            }
+        }
 
-                }
-               if (file.length() > 50)
-               {
-                    file.delete();
-               }
+        Collections.sort(resultList, new Comparator<String>()
+        {
+            @Override
+            public int compare(String o1, String o2)
+            {
+                return o1.substring(o1.lastIndexOf("/")).compareTo(o2.substring(o2.lastIndexOf("/")));
+            }
+            //sorting array by file names excluding preceeded path
+        });
+
+        FileOutputStream fileOutputStream;
+        try
+        {
+            File renamedFile = renameFile(new File(resultFileName));
+            fileOutputStream = new FileOutputStream(renamedFile);
+
+            for (String el: resultList)
+            {
+                FileInputStream fileInput = new FileInputStream(new File(el));
+                byte [] buffer = new byte[fileInput.available()];
+                fileInput.read(buffer);
+                fileOutputStream.write(buffer);
+                fileOutputStream.write('\n');
+                fileInput.close();
             }
 
         }
+        catch (IOException ie)
+        {
+            ie.printStackTrace();
+        }
+
+    }
+
+    //recursive file search
+    public static void processDirectories(String directoryPath, List<String> fileNames)
+    {
+        File file = new File(directoryPath);
+
+        for (File f: file.listFiles())
+        {
+            if (f.isDirectory())
+            {
+                if (f.list().length == 0)
+                {
+                    f.delete();
+                }
+                else
+                {
+                    processDirectories(f.getAbsolutePath(),fileNames);
+                }
+            }
+            else
+            {
+                fileNames.add(f.getAbsolutePath());
+            }
+        }
+    }
+
+
+    public static File renameFile(File file) throws IOException
+    {
+        String newFilePath = file.getParentFile().getAbsolutePath()+"/allFilesContent.txt";
+        File renamedFile = new File(newFilePath);
+        if (renamedFile.exists())
+        {
+            throw new IOException("file exists");
+        }
+        file.renameTo(renamedFile);
+        return renamedFile;
     }
 }
