ff44d6ebf085f4c1953900d7d26599259af93692
stanislasdp <forempty@gmail.com>
2016-09-14 15:24:20 +0300

    Update Restaurant.java

--- src/com/javarush/test/level27/lesson15/big01/Restaurant.java
+++ src/com/javarush/test/level27/lesson15/big01/Restaurant.java
@@ -10,59 +10,28 @@ import java.util.Locale;
  */
 public class Restaurant
 {
-    private static  int ORDER_CREATING_INTERVAL = 100;
-
-    public static void main(String[] args)
-    {
-        Tablet tablet = new Tablet(5);
-        Cook cook1 = new Cook("Amigo");
-        Waitor waitor = new Waitor();
-        tablet.addObserver(cook1);
-        cook1.addObserver(waitor);
-        tablet.createOrder();
-        DirectorTablet directorTablet = new DirectorTablet();
-        directorTablet.printAdvertisementProfit();
-        directorTablet.printCookWorkloading();
-        directorTablet.printActiveVideoSet();
-        directorTablet.printArchivedVideoSet();
-
-    /*    Locale.setDefault(Locale.ENGLISH);
-        Tablet tablet = new Tablet(5);
-        Cook amigo = new Cook("Amigo");
-        Waitor waitor = new Waitor();
-        amigo.addObserver(waitor);
-        tablet.addObserver(amigo);
-        tablet.createOrder();
-        DirectorTablet directorTablet = new DirectorTablet();
-        directorTablet.printAdvertisementProfit();
-        directorTablet.printCookWorkloading();
-        directorTablet.printActiveVideoSet();
-        directorTablet.printArchivedVideoSet();
-        Cook cook2=new Cook("mAmigo");
-        Tablet tablet2=new Tablet(5);
-        Waitor waiter2=new Waitor();
-        cook2.addObserver(waiter2);
-        tablet2.addObserver(cook2);
-        tablet2.createOrder();
-        directorTablet=new DirectorTablet();
-        directorTablet.printAdvertisementProfit();
-        directorTablet.printCookWorkloading();
-        directorTablet.printActiveVideoSet();
-        directorTablet.printArchivedVideoSet();
-        Cook cook3=new Cook("Am");
-        Tablet tablet3=new Tablet(5);
-        Waitor waiter3=new Waitor();
-        cook3.addObserver(waiter3);
-        tablet3.addObserver(cook3);
-        tablet3.createOrder();
-        directorTablet=new DirectorTablet();
-        directorTablet.printAdvertisementProfit();
-        directorTablet.printCookWorkloading();
-        directorTablet.printActiveVideoSet();
-        directorTablet.printArchivedVideoSet();
-*/
-
-    }
+    private static int ORDER_CREATING_INTERVAL = 100;
+	
+	    public static void main(String[] args)
+	    {
+	        Tablet tablet1 = new Tablet(5);
+	        Tablet tablet2 = new Tablet(6);
+	        List<Tablet>  tabletsList = new ArrayList<>();
+	       tabletsList.add(tablet1);
+	       tabletsList.add(tablet2);
+	       Cook cook1 = new Cook("Amigo");
+	       Waitor waitor = new Waitor();
+	       tablet1.addObserver(cook1);
+	       tablet2.addObserver(cook1);
+	       cook1.addObserver(waitor);
+	       Thread th = new Thread(new RandomOrderGeneratorTask(tabletsList, ORDER_CREATING_INTERVAL));
+	       th.start();
+	        DirectorTablet directorTablet = new DirectorTablet();
+	        directorTablet.printAdvertisementProfit();
+	        directorTablet.printCookWorkloading();
+	        directorTablet.printActiveVideoSet();
+	        directorTablet.printArchivedVideoSet();
+	    }
 }
 
 
