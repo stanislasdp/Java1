7f77b8ae04ca09ffe4a3759f50687df1f104f957
stanislasdp <forempty@gmail.com>
2016-09-15 16:35:09 +0300

    Update Tablet.java

--- src/com/javarush/test/level27/lesson15/big01/Tablet.java
+++ src/com/javarush/test/level27/lesson15/big01/Tablet.java
@@ -16,73 +16,69 @@ import java.util.logging.Logger;
  * Created by stas on 9/10/16.
  */
 public class Tablet
-{
-    private static final Logger logger = Logger.getLogger(Tablet.class.getName());
-    private final int number;
-    private LinkedBlockingQueue<Order> orderQueu;
+{private static final Logger logger = Logger.getLogger(Tablet.class.getName());
+	    private final int number;
+	    private LinkedBlockingQueue<Order> queue;
 
-    public void setOrderQueu(LinkedBlockingQueue<Order> orderQueu)
-    {
-        this.orderQueu = orderQueu;
-    }
-
-
-
-    public Tablet(int number)
-    {
-        this.number = number;
-    }
+	    public void setQueue(LinkedBlockingQueue<Order> queue)
+	    {
+	        this.queue = queue;
+	    }
+	    
+	    public Tablet(int number)
+	    {
+	        this.number = number;
+	    }
 
 
-    @Override
-    public String toString()
-    {
-        return String.format("Tablet{number=%d}",number);
-    }
+	    @Override
+	    public String toString()
+	    {
+	        return String.format("Tablet{number=%d}",number);
+	    }
 
-    public void createOrder()
-    {
-        try
-        {
-            orderJob(new Order(this));
-        }
-        catch (IOException ie)
-        {
-            logger.log(Level.SEVERE,"Console is unavailable.");
-        }
-    }
+	    public void createOrder()
+	    {
+	        try
+	        {
+	            orderJob(new Order(this));
+	        }
+	        catch (IOException ie)
+	        {
+	            logger.log(Level.SEVERE,"Console is unavailable.");
+	        }
+	    }
 
-    public void createTestOrder()
-    {
-        try
-        {
-            orderJob(new TestOrder(this));
-        }
-        catch (IOException ie)
-        {
-            logger.log(Level.SEVERE,"Console is unavailable.");
-        }
+	    public void createTestOrder()
+	    {
+	        try
+	        {
+	            orderJob(new TestOrder(this));
+	        }
+	        catch (IOException ie)
+	        {
+	            logger.log(Level.SEVERE,"Console is unavailable.");
+	        }
 
-    }
+	    }
 
-    private void orderJob(Order order)
-    {
-        try
-        {
-            ConsoleHelper.writeMessage(order.toString());
+	    private void orderJob(Order order)
+	    {
+	        try
+	        {
+	            ConsoleHelper.writeMessage(order.toString());
 
-            if (!order.isEmpty())
-            {
-                orderQueu.add(order);
-                new AdvertisementManager(order.getTotalCookingTime() * 60).processVideos();
-            }
-        }
+	            if (!order.isEmpty())
+	            {
+	                queue.add(order);
+	                //add new order to queue
+	                new AdvertisementManager(order.getTotalCookingTime() * 60).processVideos();
+	            }
+	        }
 
-        catch (NoVideoAvailableException ne)
-        {
-            logger.log(Level.INFO,"No video is available for the order ");
-        }
-    }
-
-
+	        catch (NoVideoAvailableException ne)
+	        {
+	            logger.log(Level.INFO,"No video is available for the order ");
+	    
+	        }
 }
