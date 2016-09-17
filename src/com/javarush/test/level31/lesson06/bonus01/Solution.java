932b4c2ce6bca3242025a3e3a1d39acaf37d07b1
stas <forempty@gmail.com>
2016-09-17 16:12:54 +0300

    aaa

--- src/com/javarush/test/level31/lesson06/bonus01/Solution.java
+++ src/com/javarush/test/level31/lesson06/bonus01/Solution.java
@@ -1,5 +1,10 @@
 package com.javarush.test.level31.lesson06.bonus01;
 
+import java.io.*;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 /* Разархивируем файл
 В метод main приходит список аргументов.
 Первый аргумент - имя результирующего файла resultFileName, остальные аргументы - имена файлов fileNamePart.
@@ -16,6 +21,97 @@ C:/pathToTest/test.zip.004
 C:/pathToTest/test.zip.002
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+       ZipFilesContainer zipParts = getZipContainer();
+
+        for (int i = 1; i <args.length ; i++)
+        {
+           zipParts.addFilePart(args[i]);
+        }
+        zipParts.sortFiles();
+
+        try
+        {
+            Enumeration< ? extends InputStream> en = Collections.enumeration(Arrays.asList(zipParts.getInputStreams()));
+            ZipInputStream so = new ZipInputStream(new SequenceInputStream(en));
+
+            BufferedOutputStream bo = new BufferedOutputStream(new FileOutputStream(args[0]));
+
+            ZipEntry zipEntry = null;
+            while ((zipEntry =so.getNextEntry()) != null)
+            {
+                final int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+
+                for (int readBytes = -1;(readBytes = so.read(buffer,0,bufferSize)) > -1;)
+                {
+                   bo.write(buffer,0,readBytes);
+                }
+            }
+
+            so.close();
+            bo.close();
+
+        }
+        catch (IOException ie)
+        {
+            ie.printStackTrace();
+        }
+
+    }
+
+
+    public static void writeToFile(String fileName, byte[] array) throws IOException
+    {
+        File file = new File(fileName);
+        file.createNewFile();
+        FileOutputStream fo = new FileOutputStream(new File(fileName));
+        fo.write(array);
+        fo.close();
+    }
+
+    public static ZipFilesContainer getZipContainer()
+    {
+        return new ZipFilesContainer();
+    }
+
+    private static class ZipFilesContainer
+    {
+        private LinkedList<File> files = new LinkedList<>();
+
+        private void sortFiles ()
+        {
+            Collections.sort(files, new Comparator<File>()
+            {
+                @Override
+                public int compare(File o1, File o2)
+                {
+                    String firstName = o1.getName();
+                    String secondName = o2.getName();
+                    return firstName.substring(firstName.lastIndexOf(".")).compareTo(secondName.substring(secondName.lastIndexOf(".")));
+                }
+            });
+        }
+
+        private void addFilePart(String filePartName)
+        {
+            files.add(new File(filePartName));
+
+        }
+
+        private FileInputStream[] getInputStreams () throws IOException
+        {
+            FileInputStream[] fisArr = new FileInputStream[files.size()];
+
+            for (int i = 0; i <files.size() ; i++)
+            {
+                fisArr[i] = new FileInputStream(files.get(i));
+            }
+
+            return  fisArr;
+        }
+
+
     }
 }
