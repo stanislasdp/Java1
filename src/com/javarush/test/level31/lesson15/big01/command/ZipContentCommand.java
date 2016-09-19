2fcc669fa517a7fd1ef9dcdb91aaa5e03acf7e3a
stanislasdp <forempty@gmail.com>
2016-09-19 16:57:34 +0300

    14

--- src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
@@ -5,9 +5,17 @@ package com.javarush.test.level31.lesson15.big01.command;
  */
 public class ZipContentCommand extends ZipCommand
 {
-    @Override
+     @Override
     public void execute() throws Exception
     {
-
+    	ZipFileManager zipFileManager = getZipFileManager();
+    	ConsoleHelper.writeMessage("Содержимое архива:");
+    	List<FileProperties> fileList = zipFileManager.getFilesList();
+    	
+    	for (FileProperties fileProperties : fileList) 
+    	{
+    		ConsoleHelper.writeMessage(fileProperties.toString());
+		}
+    	ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
