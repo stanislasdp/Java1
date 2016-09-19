5d4a1db5bb6ebf20940d5cf3ffc3b311120c8838
stanislasdp <forempty@gmail.com>
2016-09-19 18:09:53 +0300

    15

--- src/com/javarush/test/level31/lesson15/big01/command/ZipExtractCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipExtractCommand.java
@@ -8,6 +8,22 @@ public class ZipExtractCommand extends ZipCommand
     @Override
     public void execute() throws Exception
     {
+    	
+    	 try {
+             ConsoleHelper.writeMessage("Распаковка архива.");
 
+             ZipFileManager zipFileManager = getZipFileManager();
+
+             ConsoleHelper.writeMessage("Введите полное имя файла или директории для распаковки:");
+             Path outputPath = Paths.get(ConsoleHelper.readString());
+             zipFileManager.extractAll(outputPath);
+
+             ConsoleHelper.writeMessage("Распаковано");
+
+         } catch (PathIsNotFoundException e)
+    	 {
+             ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+            // e.printStackTrace();
+         }
     }
 }
