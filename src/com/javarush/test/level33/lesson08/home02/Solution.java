d8228c5103d04c57e2c2fea1c931f743de1f71b0
stas <forempty@gmail.com>
2016-09-21 22:35:03 +0300

    16

---
+++ src/com/javarush/test/level33/lesson08/home02/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level33.lesson08.home02;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.IOException;
+
+/* Десериализация XML объекта
+В метод convertFromXmlToNormal первым параметром приходит строка, содержащая xml объект.
+Вторым параметром приходит имя класса, объект которого необходимо вернуть.
+Метод convertFromXmlToNormal должен создать объект из xml-строки и вернуть его.
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException, JAXBException {
+        String xmlData = "<cat><name>Murka</name><age>5</age><weight>4</weight></cat>";
+        Cat cat = convertFromXmlToNormal(xmlData, Cat.class);
+        System.out.println(cat);
+    }
+
+    public static <T> T convertFromXmlToNormal(String xmlData, Class<T> clazz) throws IOException, JAXBException {
+        return null;
+    }
+
+    @XmlType(name = "cat")
+    @XmlRootElement
+    public static class Cat
+    {
+        public String name;
+        public int age;
+        public int weight;
+
+        @Override
+        public String toString() {
+            return "Cat{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", weight=" + weight +
+                    '}';
+        }
+    }
+}
