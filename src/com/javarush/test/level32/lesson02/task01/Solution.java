7dc83964a8f3edf3e719ec7c18974beb803b7f81
stas <forempty@gmail.com>
2016-09-21 08:53:33 +0300

    16

---
+++ src/com/javarush/test/level32/lesson02/task01/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level32.lesson02.task01;
+
+/* Запись в файл
+В метод main приходят три параметра:
+1) fileName - путь к файлу
+2) number - число, позиция в файле
+3) text - текст
+Записать text в файл fileName начиная с позиции number.
+Если файл слишком короткий, то записать в конец файла.
+*/
+public class Solution {
+    public static void main(String... args) {
+    }
+}
---
+++ src/com/javarush/test/level32/lesson06/task01/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level32.lesson06.task01;
+
+import java.io.ByteArrayOutputStream;
+
+/* Генератор паролей
+Реализуйте логику метода getPassword, который должен возвращать ByteArrayOutputStream, в котором будут байты пароля.
+Требования к паролю:
+1) 8 символов
+2) только цифры и латинские буквы разного регистра
+3) обязательно должны присутствовать цифры, и буквы разного регистра
+Все сгенерированные пароли должны быть уникальные.
+Пример правильного пароля:
+wMh7SmNu
+*/
+public class Solution {
+    public static void main(String[] args) {
+        ByteArrayOutputStream password = getPassword();
+        System.out.println(password.toString());
+    }
+
+    public static ByteArrayOutputStream getPassword() {
+        return null;
+    }
+}
