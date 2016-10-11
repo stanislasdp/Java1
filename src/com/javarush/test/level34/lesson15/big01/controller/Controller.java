baea049bc9b8da40aae52d6969699fa3722c30dd
stanislasdp <forempty@gmail.com>
2016-10-11 12:37:42 +0300

    12

--- src/com/javarush/test/level34/lesson15/big01/controller/Controller.java
+++ src/com/javarush/test/level34/lesson15/big01/controller/Controller.java
@@ -7,21 +7,28 @@ import com.javarush.test.level34.lesson15.big01.view.View;
 /**
  * Created by stas on 10/10/16.
  */
-public class Controller implements EventListener
+
+   public class Controller implements EventListener
 {
-    private View view;
+	private View view;
     private Model model;
 
     public Controller()
     {
-        view = new View(this);
+    	 model = new Model();
+    	 model.restart();
+         view = new View(this);
         view.init();
-        model = new Model();
     }
 
     public static void main(String[] args)
     {
         Controller controller = new Controller();
+    }
+    
+   public GameObjects getGameObjects()
+    {
+    	return model.getGameObjects();
     }
 
     @Override
@@ -44,7 +51,5 @@ public class Controller implements EventListener
 
     @Override
     public void levelCompleted(int level)
-    {
-
-    }
+    {}
 }
