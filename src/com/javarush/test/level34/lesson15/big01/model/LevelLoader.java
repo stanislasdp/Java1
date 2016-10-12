601898b27763a93d9db4b2867dd6dd963dbc92e6
stas <forempty@gmail.com>
2016-10-12 23:57:56 +0300

    4

--- src/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
+++ src/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
@@ -25,6 +25,7 @@ public class LevelLoader
             tmpLevel = 60;
         }
         int cellSize = Model.FIELD_SELL_SIZE;
+        // object in tht left corner has following coordinates:
         int x0 = cellSize / 2;
         int y0 = cellSize / 2;
 
@@ -36,17 +37,21 @@ public class LevelLoader
 
         try (BufferedReader bf = new BufferedReader(new FileReader(levels.toFile()));)
         {
-
+            //find the line with corresponding level number
             while (!bf.readLine().contains("Maze: " + tmpLevel));
 
+            //skip redundant lines with offset till line with height
             bf.readLine();
             bf.readLine();
-            //skip redundant lines with offset
+
             String lineWithHeigth = bf.readLine();
+            // get height of level map, tha means numbrt of lines which need to be read
             int length = Integer.parseInt(lineWithHeigth.substring(lineWithHeigth.lastIndexOf(" ")+1));
+            //skip redundant lines till beginning of level scheme
             bf.readLine();
             bf.readLine();
             bf.readLine();
+
             for (int i=0;i<length;i++)
             {
                 String read = bf.readLine();
@@ -64,6 +69,7 @@ public class LevelLoader
                             homes.add(new Home(x0 + j * cellSize, y0 + i *cellSize));
                             break;
                         case '&':
+                            // box on home - add elements either in homes and boxes collections
                             boxes.add(new Box(x0+ j * cellSize, y0 + i * cellSize));
                             homes.add(new Home(x0 + j * cellSize, y0 + i *cellSize));
                             break;
